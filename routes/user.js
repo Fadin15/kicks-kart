c3ad0581062ecbda2e8b11a2e464a8e3bd5e80f0
fadin <fadinahammed@gmail.com>
2022-05-09 23:51:06 +0530

    user valid or invalid

--- routes/user.js
+++ routes/user.js
@@ -16,7 +16,14 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/login',(req,res)=>{
-  res.render('user/login')
+  if(req.session.loggedIn){
+    res.redirect('/')
+  }else{
+
+  res.render('user/login',{"loginErr":req.session.loginErr})
+
+  req.session.loginErr=false
+}
 })
 
 router.get('/signup',(req,res)=>{
@@ -34,6 +41,7 @@ router.post('/login',(req,res)=>{
       req.session.user=response.user
       res.redirect('/')
     }else{
+      req.session.loginErr=true
       res.redirect('/login')
     }
   })
