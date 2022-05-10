7a9a2e3dc164812d1cde922b3d3dc506a3ddcdc3
fadin <fadinahammed@gmail.com>
2022-05-10 12:26:10 +0530

    edit button on products in admin

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -23,5 +23,25 @@ module.exports={
                 resolve(response)
             })
         })
+    },
+    getProductDetails:(proId)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(proId)}).then((product)=>{
+                resolve(product)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION).updateOne({_id:objectId(proId)},{
+                $set:{
+                    name:proDetails.name,
+                    Description:proDetails.Description,
+                    category:proDetails.category
+                }
+            }).then((response)=>{
+                resolve()
+            })
+        })
     }
 }
